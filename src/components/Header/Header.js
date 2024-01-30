f4003b0c8904b27504812e0f8fa352c2aedf34ec
Deldore <103668969+Deldore@users.noreply.github.com>
2024-01-30 10:35:26 +0300

    12334

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,23 +4,7 @@ import s from './Header.module.css'
 var collapseOpened = false;
 
 const collapseState = () => {
-  if(collapseOpened) {
-    collapseOpened = false
-    collapseClose();
-  } else {
-    collapseOpened = true
-    collapseOpen();
-  }
-}
-
-const collapseOpen = () => {
-  document.getElementById(s['nav-collapse']).style.display = 'block';
-  // document.getElementById(s['nav-collapse']).style.height = 'auto';
-}
-
-const collapseClose = () => {
-  // document.getElementById(s['nav-collapse']).style.height = '0';
-  document.getElementById(s['nav-collapse']).style.display = 'none';
+  collapseOpened = !collapseOpened;
 }
 
 const Header = () => {
@@ -28,12 +12,12 @@ const Header = () => {
     return (
       <nav className={`${s.navbar} row`}>
         <div className={`col-lg-2 col-12 ${s['logo-block']}`}>
-          <div className='btn' id={s.menuButton} onClick={function(){
+          <div className={`btn ${s.menuButton}`} onClick={function(){
             collapseState();
           }}>≡</div>
           <div id={s.logo}></div>
         </div>
-        <div className='col-lg-9 col-12' id={s['nav-collapse']}>
+        <div className={`col-lg-9 col-12 ${s['nav-collapse-class']} ${collapseOpened ? "opened" : ""}`}>
           <div className={s['navbar-collapse']}>
             <a href='#' className={`${s['nav-link']} ${s['animatedLink']}`}>Sites</a>
             <a href='#' className={`${s['nav-link']} ${s['animatedLink']}`}>Telegram Bots</a>
